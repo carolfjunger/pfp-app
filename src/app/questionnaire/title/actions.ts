d04639c269204a89896d91b093b7aaf499838fc8
Ana Carolina Junger <carol@exacta.inf.puc-rio.br>
2024-06-08 13:44:54 -0300

    create title page

---
+++ src/app/questionnaire/title/actions.ts
@@ -0,0 +1,28 @@
+'use server'
+
+import prisma from "@/lib/prisma";
+
+export async function getQuestionToStartTitleQuestionnaire(){
+  try{
+    const title = await prisma.question_group.findFirst({
+      where: {
+        topic: 'title'
+      },
+      include: {
+        question: {
+          where: {
+            is_first_question: true
+          },
+          include: {
+            option: true
+          }
+        }
+      }
+    })
+    const firstQuestion =  title?.question
+    return firstQuestion ? firstQuestion[0] : null
+  }catch(e){
+    console.log({ e })
+  }
+}
+
---
+++ src/app/questionnaire/title/page.tsx
@@ -0,0 +1,38 @@
+'use client'
+import { useEffect, useState } from "react";
+import { useSearchParams } from "next/navigation";
+import QuestionInput from "@/components/QuestionInput";
+import { getQuestionToStartTitleQuestionnaire } from "./actions";
+
+
+export default function TitlePage(){
+  const [question, setQuestion] = useState<any>(null)
+  const [isLoadingQuestion, setIsLoadingQuestion] = useState(true)
+  const searchParams = useSearchParams()
+  const visualizationId = searchParams.get('visualizationId')
+
+  useEffect(() => {
+    async function fetchQuestion() {
+      const firstQuestion  = await getQuestionToStartTitleQuestionnaire()
+      setQuestion(firstQuestion)
+      setIsLoadingQuestion(false)
+    } 
+    fetchQuestion()
+  }, [])
+
+
+  const text = question?.text
+  const option = question?.option
+  const optionType = question?.option[0]?.type
+
+  
+  return (
+    <QuestionInput
+      isLoadingQuestion={isLoadingQuestion}
+      optionType={optionType}
+      question={question}
+      visualizationId={Number(visualizationId)}
+      optionId={option?.length ? option[0].id : -1}
+    />
+  )
+}
